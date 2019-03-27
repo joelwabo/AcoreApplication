54715ecd7a762789ad6c07f2b39d92f69c9f99f3
joelwabo <joelwabo@yahoo.fr>
2019-03-27 15:40:25 +0100

    c

--- AcoreApplication/Model/Historique.cs
+++ AcoreApplication/Model/Historique.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.SqlClient;
-using static AcoreApplication.Model.Constantes;
-using System.Collections.ObjectModel;
-using static AcoreApplication.Model.Redresseur;
-using System.Threading;
+﻿using System.Threading;
 
 namespace AcoreApplication.DataService
 {
